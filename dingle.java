19ab695c0621af7c63a307ed3ed7a3aad1cf311c
Arjun <arjung9893@gmail.com>
2025-11-14 12:14:21 -0500

    Added comments
    Added comments to explain each method and some extra code

--- dingle.java
+++ dingle.java
@@ -2,13 +2,15 @@ import java.util.*;
 public class dingle{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter an integer: ");
+        System.out.println("Please enter an integer: "); // User inputs size for square array
         int size = scan.nextInt();
 
         int[][] arr = fillSquareArray(size);
         System.out.println("\nPrint first even number: " + returnEven(arr));
     }
 
+    // Creates a square array of size x by x (user input)
+    // Fills it with random numbers from 1-10
     public static int[][] fillSquareArray(int x){
         int[][] arr = new int[x][x];
 
@@ -20,6 +22,8 @@ public class dingle{
         return arr;
     }
 
+    // Returns first even number in the array
+    // Loops through it in row major order
     public static int returnEven(int[][] arr){
         for(int i = 0; i<arr.length; i++){
             for(int j = 0; j<arr[i].length; j++){
